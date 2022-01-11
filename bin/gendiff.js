f520bcb345d97c6a60236e66ab6b90c27c8e359b
n3imys <danik.neimys@gmail.com>
2022-01-12 00:40:13 +0300

    1

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const  program  = require('commander');
+
+program
+    .usage('[options] <filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .version('0.0.1')
+    .option('-f, --format [type]', 'output format')
+    .parse(process.argv);
